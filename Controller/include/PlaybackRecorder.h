0217371632b15a1bd69a175940da14936881fd53
Brandon To <brandon_to@hotmail.com>
2016-02-19 19:18:40 -0500

    Started PlaybackRecorder class. Working on integrating with rest of code base.

---
+++ Controller/include/PlaybackRecorder.h
@@ -0,0 +1,42 @@
+//*****************************************************************************
+//
+// PlaybackRecorder.h
+//
+// Class responsible for recording data stream and saving to disk.
+//
+// Copyright (c) 2015 Brandon To, Minh Mai, and Yuzhou Liu
+// This code is licensed under BSD license (see LICENSE.txt for details)
+//
+// Created:
+// Feburary 19, 2016
+//
+// Modified:
+// Feburary 19, 2016
+//
+//*****************************************************************************
+#ifndef _PLAYBACKRECORDER_H_
+#define _PLAYBACKRECORDER_H_
+
+#include <fstream>
+
+class PlaybackRecorder
+{
+private:
+    /* Fields */
+    std::ofstream _file;
+    bool _recording;
+
+public:
+    /* Constructor */
+    PlaybackRecorder();
+
+    /* Destructor */
+    ~PlaybackRecorder();
+
+    /* Methods */
+    bool startRecording(char *filePath, int fps);
+    bool stopRecording();
+    void update(char *data);
+};
+
+#endif /* _PLAYBACKRECORDER_H_ */
---
+++ Controller/src/PlaybackRecorder.cpp
@@ -0,0 +1,134 @@
+//*****************************************************************************
+//
+// PlaybackRecorder.cpp
+//
+// Class responsible for recording data stream and saving to disk.
+//
+// Copyright (c) 2015 Brandon To, Minh Mai, and Yuzhou Liu
+// This code is licensed under BSD license (see LICENSE.txt for details)
+//
+// Created:
+// Feburary 19, 2016
+//
+// Modified:
+// Feburary 19, 2016
+//
+//*****************************************************************************
+#include "PlaybackRecorder.h"
+
+#include <iostream>
+
+//*****************************************************************************
+//
+//! Empty constructor for PlaybackRecorder.
+//!
+//! \param None.
+//!
+//! \return None.
+//
+//*****************************************************************************
+PlaybackRecorder::PlaybackRecorder()
+    : _file(), _recording(false)
+{
+
+}
+
+//*****************************************************************************
+//
+//! Destructor for PlaybackRecorder. Releases file handle if required.
+//!
+//! \param None.
+//!
+//! \return None.
+//
+//*****************************************************************************
+PlaybackRecorder::~PlaybackRecorder()
+{
+    if (_file.is_open())
+    {
+        _file.close();
+    }
+}
+
+//*****************************************************************************
+//
+//! Starts recording data stream to file.
+//!
+//! \param filePath path to file to save data stream to.
+//!
+//! \return Returns \b true if recording started successfully and \b false
+//! otherwise.
+//
+//*****************************************************************************
+bool PlaybackRecorder::startRecording(char *filePath, int fps)
+{
+    if (_recording)
+    {
+        std::cout << "[WARNING] PlaybackRecorder::startRecording(): Already "\
+            "recording!"<< std::endl;
+        return false;
+    }
+
+    _file.open(filePath);
+    if (!_file.is_open())
+    {
+        std::cout << "[ERROR] PlaybackRecorder::startRecording(): Cannot "\
+            "open file!"<< std::endl;
+        return false;
+    }
+
+    _file << "HIRC" << fps;
+
+    _recording = true;
+    return true;
+}
+
+//*****************************************************************************
+//
+//! Stops recording data stream and save file.
+//!
+//! \param None.
+//!
+//! \return Returns \b true if recording stopped successfully and \b false
+//! otherwise.
+//
+//*****************************************************************************
+bool PlaybackRecorder::stopRecording()
+{
+    if (!_recording)
+    {
+        std::cout << "[WARNING] PlaybackRecorder::stopRecording(): Not "\
+            "recording!"<< std::endl;
+        return false;
+    }
+
+    if (!_file.is_open())
+    {
+        std::cout << "[ERROR] PlaybackRecorder::stopRecording(): File not "\
+            "open!"<< std::endl;
+        return false;
+    }
+
+    _file.close();
+    _recording = false;
+    return true;
+}
+
+//*****************************************************************************
+//
+//! Appends data to file if recording. Called once per frame.
+//!
+//! \param None.
+//!
+//! \return None.
+//
+//*****************************************************************************
+void PlaybackRecorder::update(char *data)
+{
+    if (!_recording)
+    {
+        return;
+    }
+
+    // TODO (Brandon): Append data to file
+}
