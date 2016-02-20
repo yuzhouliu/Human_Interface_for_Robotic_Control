1e2e834e78c2c4ee31a7b09444c483caea7e9ccd
Brandon To <brandon_to@hotmail.com>
2016-02-19 20:31:19 -0500

    Started PlaybackStreamer class for streaming data from file.

---
+++ Controller/include/PlaybackStreamer.h
@@ -0,0 +1,42 @@
+//*****************************************************************************
+//
+// PlaybackStreamer.h
+//
+// Class responsible for streaming recorded data from file.
+//
+// Copyright (c) 2015 Brandon To, Minh Mai, and Yuzhou Liu
+// This code is licensed under BSD license (see LICENSE.txt for details)
+//
+// Created:
+// Feburary 19, 2016
+//
+// Modified:
+// Feburary 19, 2016
+//
+//*****************************************************************************
+#ifndef _PLAYBACKSTREAMER_H_
+#define _PLAYBACKSTREAMER_H_
+
+#include <fstream>
+
+class PlaybackStreamer
+{
+private:
+    /* Fields */
+    std::ifstream _file;
+    bool _streaming;
+
+public:
+    /* Constructor */
+    PlaybackStreamer();
+
+    /* Destructor */
+    ~PlaybackStreamer();
+
+    /* Methods */
+    bool isStreaming();
+    bool startStreaming(char *filePath);
+    bool stopStreaming();
+};
+
+#endif /* _PLAYBACKSTREAMER_H_ */
---
+++ Controller/src/PlaybackStreamer.cpp
@@ -0,0 +1,93 @@
+
+//
+// PlaybackStreamer.cpp
+//
+// Class responsible for streaming recorded data from file.
+//
+// Copyright (c) 2015 Brandon To, Minh Mai, and Yuzhou Liu
+// This code is licensed under BSD license (see LICENSE.txt for details)
+//
+// Created:
+// Feburary 19, 2016
+//
+// Modified:
+// Feburary 19, 2016
+//
+//*****************************************************************************
+#include "PlaybackStreamer.h"
+
+//*****************************************************************************
+//
+//! Empty constructor for PlaybackStreamer.
+//!
+//! \param None.
+//!
+//! \return None.
+//
+//*****************************************************************************
+PlaybackStreamer::PlaybackStreamer()
+    : _file(), _recording(false)
+{
+
+}
+
+//*****************************************************************************
+//
+//! Destructor for PlaybackStreamer. Releases file handle if required.
+//!
+//! \param None.
+//!
+//! \return None.
+//
+//*****************************************************************************
+PlaybackStreamer::~PlaybackStreamer()
+{
+    if (_file.is_open())
+    {
+        _file.close();
+    }
+}
+
+//*****************************************************************************
+//
+//! Returns true if streaming in progress.
+//!
+//! \param None.
+//!
+//! \return Returns \b true if streaming in progress and \b false otherwise.
+//
+//*****************************************************************************
+bool PlaybackStreamer::isStreaming()
+{
+    return _streaming;
+}
+
+//*****************************************************************************
+//
+//! Starts streaming data from file.
+//!
+//! \param filePath path to file to stream data from.
+//!
+//! \return Returns \b true if streaming started successfully and \b false
+//! otherwise.
+//
+//*****************************************************************************
+bool PlaybackStreamer::startStreaming(char *filePath)
+{
+    // TODO (Brandon): Implement
+}
+
+//*****************************************************************************
+//
+//! Stops streaming data.
+//!
+//! \param None.
+//!
+//! \return Returns \b true if streaming stopped successfully and \b false
+//! otherwise.
+//
+//*****************************************************************************
+bool stopStreaming()
+{
+    // TODO (Brandon): Implement
+}
